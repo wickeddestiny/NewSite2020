c94f9e69d22fa95531eac203616dad3b0142ecaf
wickeddestiny <michajlo.rakonjac@gmail.com>
2020-12-09 16:06:24 +0100

    Auto Commit Brba

--- main.js
+++ main.js
@@ -5,30 +5,31 @@ let signField = $("#signinField").css("visibility","hidden");
 let logField = $("#loginField").css("visibility","visible");
 
 
-//          Prelazak u DarkMode
-$(whiteImg).click(function(){
-    if(whiteImg){
-        whiteImg.hide();
-        blackImg.css("visibility","visible");
-        $("#darkMode").attr("href","dark.css")
-    }
-    else{
 
-    }
-});
+//          Prelazak u DarkMode
+    $(whiteImg).click(function(){
+        if(whiteImg){
+            whiteImg.hide();
+            blackImg.css("visibility","visible");
+            $("#darkMode").attr("href","dark.css")
+        }
+        else{
+
+        }
+    });
 
 //          Prelazak u LightMode
+    $(blackImg).click(function(){
+        if(blackImg){
+            blackImg.css("visibility","hidden");
+            whiteImg.show();
+            $("#darkMode").attr("href","style.css")
+        }
+        else{
 
-$(blackImg).click(function(){
-    if(blackImg){
-        blackImg.css("visibility","hidden");
-        whiteImg.show();
-        $("#darkMode").attr("href","style.css")
-    }
-    else{
+        }
+    });
 
-    }
-});
 //           SignIn-BTN-Active
 function  signActive () {
     $(".btnSign").click(function(){
@@ -44,7 +45,6 @@ function  signActive () {
         }
     });
 }
-
 
 //          LogIn-BTN-Active
 function logActive (){
@@ -63,16 +63,57 @@ function logActive (){
     });
 }
 
+//           Show Password Sign In
 
+    $("#checkLogBtn").click(function(){
+        if ("password" == $("#passLog").attr("type")){
+            $("#passLog").prop("type","text")
+        }
+        else{
+            $("#passLog").prop("type","password")
+        }
+    });
+
+//           Show Password Log In
+
+    $("#checkSignBtn").click(function(){
+        if ("password" === $("#passSign").attr("type")){
+            $("#passSign").prop("type","text")
+            $("#passRepeat").prop("type","text")
+        }
+        else{
+            $("#passSign").prop("type","password")
+            $("#passRepeat").prop("type","password")
+        }
+    });
+
+//           Sign Up BTN  -  Password / Repeated Checker
+
+   
+    
+    $(function validate() {
+        $("#signInBtn").click(function () {
+            
+            const passInput = $("#passSign").val();
+            const passRepeat = $("#passRepeat").val();
+            if (passInput !== passRepeat) {
+                alert("Passwords do not match.");
+                return false
+            }
+            else{
+                return true
+            }
+        });
+    });
 
 
 //           Function-After-Sign/Log Page Click
 
-$("#logSignPage").click(function(){
-    if("logSignPage"){
-        logActive()
-    }
-});
+    $("#logSignPage").click(function(){
+        if("logSignPage"){
+            logActive()
+        }
+    });
 
 
 
